e7e62098e9177e1eb4bd0ce9c186e57e330e4754
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-06-29 23:54:48 +0900

    ⚡️ brush up navbar

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,10 +1,54 @@
-import React from 'react';
-import './navbar.css';
+import React from "react";
+import "./navbar.css";
+import SearchIcon from "@mui/icons-material/Search";
+import LanguageIcon from "@mui/icons-material/Language";
+import NightlightIcon from "@mui/icons-material/Nightlight";
+import FullscreenExitIcon from "@mui/icons-material/FullscreenExit";
+import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
 
 function Navbar() {
   return (
-    <div className='navbar'>Navbar</div>
-  )
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <SearchIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageIcon className="icon" />
+            English/Japanese
+          </div>
+          <div className="item">
+            <NightlightIcon className="icon" />
+          </div>
+          <div className="item">
+            <FullscreenExitIcon className="icon" />
+          </div>
+          <div className="item">
+            <NotificationsActiveIcon className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineIcon className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <FormatListBulletedIcon className="icon" />
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/1182125/pexels-photo-1182125.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt="Photo of me"
+              className="avatar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
