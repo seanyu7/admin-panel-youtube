71f60747c64d4401401b5ce367d4b4f92b52f81a
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-06-29 23:56:23 +0900

    ⚡️ adding sidebar and navbar component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
-import Navbar from '../../component/navbar/Navbar'
-import Sidebar from '../../component/sidebar/Sidebar'
-import'./home.css'
-
+import React from "react";
+import Navbar from "../../component/navbar/Navbar";
+import Sidebar from "../../component/sidebar/Sidebar";
+import Widgets from "../../component/widgets/Widgets";
+import "./home.css";
 
 function Home() {
   return (
-    <div className='home'>
-     <Sidebar/>
-     <div className='homeContainer'>container</div>
-     
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widgets type="users" />
+          <Widgets type="order" />
+          <Widgets type="erarning" />
+          <Widgets type="balance" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
