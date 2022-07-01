455b92c77db3eaf32c76685e0d98cb0b34cf0d71
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-01 18:40:10 +0900

    💄 made some change in UX

--- src/component/feature/Feature.jsx
+++ src/component/feature/Feature.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import "./feature.css";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { CircularProgressbar } from "react-circular-progressbar";
-import 'react-circular-progressbar/dist/styles.css';
+import "react-circular-progressbar/dist/styles.css";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 function Feature() {
   return (
@@ -11,13 +12,40 @@ function Feature() {
         Total revenue
         <MoreVertIcon fontSize="small" />
       </div>
+
+    
       <div className="bottom">
         <div className="featuredChart">
-          <CircularProgressbar value={70} text={"70%"} strokeWidth={5}/>
+          <CircularProgressbar value={70} text={"70%"} strokeWidth={5} />
         </div>
         <p className="title">Total sales made today</p>
         <p className="amount">$420</p>
-        <p className="discription">Previous transaction processing. Last payment may not be included.</p>
+        <p className="discription">
+          Previous transaction processing. Last payment may not be included.
+        </p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult">
+              <KeyboardArrowDownIcon fontSize="small" />
+              <div className="resultAmount">$12.4K</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult">
+              <KeyboardArrowDownIcon fontSize="small" />
+              <div className="resultAmount">$12.4K</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult">
+              <KeyboardArrowDownIcon fontSize="small" />
+              <div className="resultAmount">$12.4K</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
