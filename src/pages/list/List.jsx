ae30366ea1c16d8833c68ae0bae10394f08b418a
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-02 03:06:45 +0900

    💄 remade a code to improve UI

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import DataTable from "../../component/datatable/DataTable";
+import Navbar from "../../component/navbar/Navbar";
+import Sidebar from "../../component/sidebar/Sidebar";
 
 function List() {
   return (
-    <div>List</div>
-  )
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        <DataTable/>
+      </div>
+    </div>
+  );
 }
 
-export default List
+export default List;
