570d0823e870dc1a1a5640216eea7fb60f96fad9
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-03 00:04:37 +0900

    ⚡️ made a js to give data for products and user page

---
+++ src/formeSource.js
@@ -0,0 +1,78 @@
+export const userInputs = [
+  {
+    id: 1,
+    label: "Username",
+    type: "text",
+    placeholder: "Son Hun Min",
+  },
+  {
+    id: 2,
+    label: "Name and Sirname",
+    type: "text",
+    placeholder: "Son Hun Min",
+  },
+  {
+    id: 3,
+    label: "Email",
+    type: "mail",
+    placeholder: "yuinz2@gmail.com",
+  },
+  {
+    id:4,
+    label:"Phone",
+    type:"text",
+    placeholder:"080-1231-4564",
+  },
+  {
+    id:5,
+    label:"Password",
+    type:"password",
+  },
+  {
+    id:6,
+    label:"Adress",
+    type:"text",
+    placeholder:"Shinjuku Tokyo",
+  },
+  {
+    id:7,
+    label:"Country",
+    type:"text",
+    placeholder:"Japan",
+  },
+];
+
+
+export const productInputs=[
+    {
+        id:1,
+        label:"Title",
+        type:"text",
+        placeholder:"Apple Macbook Pro",
+    },
+    {
+        id:2,
+        label:"Description",
+        type:"text",
+        placeholder:"Description",
+    },
+    {
+        id:3,
+        label:"Category",
+        type:"text",
+        placeholder:"",
+    },
+    {
+        id:4,
+        label:"Price",
+        type:"text",
+        placeholder:"1000",
+    },
+    {
+        id:5,
+        label:"Stock",
+        type:"text",
+        placeholder:"in stpck",
+
+    },
+];
