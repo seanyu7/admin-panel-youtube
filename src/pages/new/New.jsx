f693ea19fb9ebd8d7c265b1ea5bbd56d886b6cf0
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-03 00:05:16 +0900

    ♻️ refactor the code to make it easy to read

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,18 +1,49 @@
-import React from 'react'
-import Navbar from '../../component/navbar/Navbar'
-import Sidebar from '../../component/sidebar/Sidebar'
-import "./new.css"
+import React from "react";
+import Navbar from "../../component/navbar/Navbar";
+import Sidebar from "../../component/sidebar/Sidebar";
+import "./new.css";
+import DriveFolderUploadIcon from "@mui/icons-material/DriveFolderUpload";
 
-function New() {
+function New({ inputs, title }) {
   return (
-    <div className='new'>
-      <Sidebar/>
+    <div className="new">
+      <Sidebar />
       <div className="newContainer">
-        <Navbar/>
-        test
+        <Navbar />
+        <div className="top">
+          <h1>{title}</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png"
+              alt="photo of user"
+            />
+          </div>
+
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label htmlFor="file">
+                  Image:
+                  <DriveFolderUploadIcon className="icon" />
+                </label>
+                <input type="file" id="file" style={{ display: "none" }} />
+              </div>
+
+              {inputs.map((input) => (
+                <div className="formInput" key={input.id}>
+                  <label>{input.label}</label>
+                  <input type={input.type} placeholder={input.placeholder} />
+                </div>
+              ))}
+              <button>Send</button>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default New
+export default New;
