07bd17402fcc457ed2bb9960720665daaf83e287
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-02 21:18:35 +0900

    ✨ making a new components and page for this app

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Navbar from '../../component/navbar/Navbar'
+import Sidebar from '../../component/sidebar/Sidebar'
+import "./new.css"
 
 function New() {
   return (
-    <div>New</div>
+    <div className='new'>
+      <Sidebar/>
+      <div className="newContainer">
+        <Navbar/>
+        test
+      </div>
+    </div>
   )
 }
 
