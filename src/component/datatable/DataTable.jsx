78fcac31693e0d688720782ddedbe59a2f57bb41
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-02 21:12:54 +0900

    ⚡️ made a datatable component and installing data

--- src/component/datatable/DataTable.jsx
+++ src/component/datatable/DataTable.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import "./datatable.css";
+import { DataGrid } from "@mui/x-data-grid";
+import {userColumns,rows} from "../../datatablesource"
 
-function DataTable() {
-  return <div className="dataTable">DataTable</div>;
+
+function Datatable() {
+const actionColumn = [{field:"action", headerName:"Action", width:200, renderCell:()=>{
+    return(
+<div className="cellAction">
+    <button className="viewButton">View</button>
+    <button className="deleteButton">Delete</button>
+</div>
+    )
+}}]
+
+
+
+  return (
+    <div className="dataTable">
+      <DataGrid
+        rows={rows}
+        columns={userColumns.concat(actionColumn)}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
+    </div>
+  );
 }
 
-export default DataTable;
+export default Datatable;
