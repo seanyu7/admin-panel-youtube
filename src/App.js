ed026b94f5352d82ad54e49077738746921d1d0c
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-03 00:06:11 +0900

    ♻️ inserting data from file outside

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import New from "./pages/new/New";
 import Single from "./pages/single/Single";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { productInputs, userInputs } from "./formeSource";
 
 function App() {
   return (
@@ -15,15 +16,21 @@ function App() {
             <Route index element={<Home />} />
             <Route path="login" element={<Login />} />
             <Route path="users">
-               <Route index element={<List/>}/>
-               <Route path=":userId" element={<Single />} />
-            <Route path="new" element={<New />} />
-          </Route>
-          <Route path="products">
-            <Route index element={<List/>}/>
-            <Route path=":productId" element={<Single/>}/>
-            <Route path="new" element={<New/>}/>
-          </Route>
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route
+                path="new"
+                element={<New inputs={userInputs} title="Add New User" />}
+              />
+            </Route>
+            <Route path="products">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route
+                path="new"
+                element={<New inputs={productInputs} title="Add New Product" />}
+              />
+            </Route>
           </Route>
         </Routes>
       </BrowserRouter>
